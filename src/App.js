8d6f5409a0fe89235871c54ea8a0136fc3c21746
Vanya Dimitrova <vapedim@yahoo.com>
2023-03-12 22:41:53 +0200

    Add function csvFileToArrayOfObjectsGroupedByProject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React, { useState } from 'react';
 
 function App() {
-    const [file, setFile] = useState();
+    const csvDelimiter = ';';
 
-    const handelOnChange = (e) => {
-        setFile(e.target.files[0]);
-    };
+    const [file, setFile] = useState();
+    // const [table, setTable] = useState();
+
+    const tableHeaders = ['Employee ID #1', 'Employee ID #2', 'Project ID', 'Days worked'];
 
     const fileReader = new FileReader();
+
+    const handelOnChange = (e) => {
+        if (e.target.files[0].type !== 'text/csv') {
+            alert('Import .csv file');
+        } else {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const csvFileToArrayOfObjectsGroupedByProject = (string) => {
+        const header = string.slice(0, string.indexOf('\n')).split(csvDelimiter);
+        const rows = string.slice(string.indexOf('\n') + 1).split('\n').map(r => r.split(csvDelimiter));
+        
+        const objectsGroupedByProject = rows.reduce((groupedByProject, row) => {
+           const currentRow = row.reduce((obj, cell, i) => {
+                obj[header[i]] = cell;
+                return obj;
+            }, {});
+
+            const currentProjectID = currentRow['ProjectID'];
+
+            if (Object.keys(groupedByProject).includes(currentProjectID)) {
+                groupedByProject[currentProjectID].push(currentRow);
+            } else {
+                groupedByProject[currentProjectID] = [currentRow];
+            }
+
+            return groupedByProject;
+        }, {});
+
+        return objectsGroupedByProject;
+    };
+
+    //TO DO
+    const pairWithTheLongestPeriod = (string) => {
+        // const projects = csvFileToArrayOfObjectsGroupedByProject(string);
+        // console.log(Object.entries(projects));
+        // const pearsInAProject = Object.entries(projects).reduce((result, project) => {
+
+        //     console.log(project);
+        //     return result;
+        // }, []);
+
+        // return pearsInAProject;
+    };
 
     const handelOnSubmit = (e) => {
         e.preventDefault();
@@ -15,18 +61,17 @@ function App() {
         if (file) {
             fileReader.onload = function (event) {
                 const csvOutput = event.target.result;
-                console.log(csvOutput);
+                pairWithTheLongestPeriod(csvOutput);
             };
 
             fileReader.readAsText(file);
         }
-
-        setFile(null);
     };
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }} >
             <h1 style={{ marginBottom: '10px' }} >Pair of employees who have worked together</h1>
+
             <form>
                 <input 
                     id='csvFileInput'
@@ -42,6 +87,21 @@ function App() {
                     Import CSV
                 </button>
             </form>
+
+            <br />
+
+            <table>
+                <thead>
+                    <tr>
+                        {tableHeaders.map((header, i) => (
+                            <th key={i}>{header}</th>
+                        ))}
+                    </tr>
+                </thead>
+
+                <tbody>
+                </tbody>
+            </table>
         </div>
     );
 }
