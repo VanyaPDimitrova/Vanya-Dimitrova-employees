82c6194aad49f6cb64455df933bf7693f0593f25
Vanya Dimitrova <vapedim@yahoo.com>
2023-03-13 14:27:36 +0200

    - split function csvFileToArrayOfObjectsGroupedByProject in to csvFileHeaderAndRows and objectsGroupedByProject - create helpers functions: stringToDate, dateDiffInDays, cells, pairCommonDays - complete function pairWithTheLongestPeriod - create function maxPairProjects that returns rows for the resulting table - show resulting table on the screen - correct .md - add file to test in .md - change favicon and title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -8,8 +7,6 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
     <StrictMode>
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
+        <App />
     </StrictMode>
 );
